3924e3a68c04bbf75f433eaf009032fd3481fc25
Dana <qaraoygaberilme@gmail.com>
2025-01-16 22:43:29 +0500

    Lab1
    Signed-off-by: Dana <qaraoygaberilme@gmail.com>

---
+++ format/f4.py
@@ -0,0 +1,4 @@
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt) 
+#A modifier is included by adding a colon : followed by a legal formatting type, like .2f which means fixed point number with 2 decimals:
